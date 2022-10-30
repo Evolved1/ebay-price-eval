175fa268ba0276f0e083fc358429724875e34421
Evolved1 <116987302+Evolved1@users.noreply.github.com>
2022-10-30 13:48:01 +1100

    Continued separation

--- eBay_scrape.py
+++ eBay_scrape.py
@@ -4,16 +4,6 @@ import pandas as pd
 
 ERROR_0 = 'Error 0: No Data or Not Parsed Correctly'
 
-def main():
-    #search_term = "dl380p+g8" # !!For Testing (should be commented out)!! #
-    search_term = input('What item would you like to price check?\n').replace(' ', '+')
-    url = f"https://www.ebay.com.au/sch/i.html?_from=R40&_nkw='{search_term}'&_sacat=31388&LH_TitleDesc=0&LH_Sold=1&LH_Complete=1&_ipg=200&rt=nc&LH_Auction=0&_ipg=200"
-    #auction = input('Auctioned? (y/n)\n').lower()
-    print(url)
-    soup = get_data(url)
-    products = parse(soup)
-    export(products, search_term)
-
 def get_data(url):
     r = requests.get(url)
     if r.status_code != 200:
